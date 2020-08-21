55fc0a5c08a07c721741fe9cdf14beb275583ba8
valery-po <54510559+valery-po@users.noreply.github.com>
2020-08-21 22:56:49 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+   pagination: {
+    el: '.projects-pagination',
+    bulletClass: 'projects-bullet',
+				bulletActiveClass: 'projects-bullet-active',
+				clickable: true
+  },
+	autoplay: true
+});
